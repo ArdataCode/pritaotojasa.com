b7f538ce7039f0df0c7bbb1c7befed7b6fc19bc8
ArdataCode <ardata.indonesia@gmail.com>
2024-05-16 15:22:45 +0700

    mit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"*>$@.a^D8N`tp<8S3-$73kDI%hD?uX:aIv Ea)IZ8DEB>?DR4v8Udb;^e7]qeOj?";s:7:"version";s:5:"1.1.0";s:13:"attackDataKey";i:3185;s:11:"wafDisabled";b:0;}
